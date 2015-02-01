332f63f8fe63ebb51319fec68905061bc143baf9
Sohil Habib <vsohail09@gmail.com>
2015-02-01 14:48:47 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Sohil Veljee on 2/1/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
